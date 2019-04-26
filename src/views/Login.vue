bbb0b1f711b360c09478e6633d8c5a22ce9ce029
Christian Murillo <NOC-1215@neworientalclub88.local>
2019-04-26 17:16:21 +0800

    Fixed UI of Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,41 +1,43 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-form @submit.prevent="login">
-                <v-toolbar dark color="primary">
-                  <v-toolbar-title>Login Form</v-toolbar-title>
-                </v-toolbar>
-                <v-card-text>
-                  <v-text-field
-                    v-model="username"
-                    prepend-icon="person"
-                    name="username"
-                    label="Username"
-                    type="text"
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="password"
-                    prepend-icon="lock"
-                    name="password"
-                    label="Password"
-                    type="password"
-                  ></v-text-field>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" type="submit">Login</v-btn>
-                </v-card-actions>
-              </v-form>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+  <div class="login-background">
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card 
+            class="elevation-24 mx-auto"
+            color="transparent"
+            dark
+            max-width="400"
+          >
+            <v-form @submit.prevent="login">
+              <v-card-title primary-title class="justify-center">
+                <h4 class="headline font-weight-bold">Login Form</h4>
+              </v-card-title>
+              <v-card-text>
+                <v-text-field
+                  v-model="username"
+                  prepend-icon="person"
+                  name="username"
+                  label="Username"
+                  type="text"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  prepend-icon="lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions class="justify-center">
+                <v-btn block round color="secondary" type="submit">Login</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -62,3 +64,11 @@ export default {
   }
 };
 </script>
+
+<style>
+  .login-background {
+    background-image: url("../assets/login-background.jpg");
+    background-size: cover;
+    height: 100vh;
+  }
+</style>
